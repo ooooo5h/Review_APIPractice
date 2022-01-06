3631933f027823ff275f800cfb23435296321c83
전은형 <ooooo5h91@gmail.com>
2022-01-06 20:58:18 +0900

    버튼 클릭시 입력한 아이디/비번 저장

--- app/src/main/java/com/eunhyung/review_apipractice/MainActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.eunhyung.review_apipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+
+//            입력한 이메일 / 비번 변수에 저장
+            val inputEmail = edtEmail.text.toString()
+            val inputPw = edtPassword.text.toString()
+
+//            서버 API의 로그인 기능에 활용
+
+        }
     }
 }
