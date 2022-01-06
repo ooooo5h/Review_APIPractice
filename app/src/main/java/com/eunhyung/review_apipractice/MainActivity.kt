d5bfbece9f53b83d0ae0bc060481dba2376b6950
전은형 <ooooo5h91@gmail.com>
2022-01-06 21:00:00 +0900

    레트로핏 / API기능 명세 결합   - 그 결과물로, 로그인 기능 호출

--- app/src/main/java/com/eunhyung/review_apipractice/MainActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice/MainActivity.kt
@@ -2,6 +2,8 @@ package com.eunhyung.review_apipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.review_apipractice.api.APIList
+import com.eunhyung.review_apipractice.api.ServerAPI
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,12 @@ class MainActivity : AppCompatActivity() {
             val inputEmail = edtEmail.text.toString()
             val inputPw = edtPassword.text.toString()
 
-//            서버 API의 로그인 기능에 활용
+//            서버 API의 로그인 기능에 활용 (ServerAPI 클래스 / APIList 인터페이스 결합)
+            val retrofit = ServerAPI.getRetrofit()  // API 연결 도구 생성
+            val apiList = retrofit.create(APIList::class.java)  // 연결도구 + 기능목록 결합 객체 생성
+
+//            실제 로그인 기능 호출(Request)
+            apiList.postRequestLogin(inputEmail, inputPw)
 
         }
     }
