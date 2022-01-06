68c809e1cd7e1553fa23314c67cc35b0aeaeb431
전은형 <ooooo5h91@gmail.com>
2022-01-06 20:38:05 +0900

    레트로핏 객체 생성 기능을 가지게 될 클래스 ServerAPI 클래스 제작중

---
+++ app/src/main/java/com/eunhyung/review_apipractice/api/ServerAPI.kt
@@ -0,0 +1,15 @@
+package com.eunhyung.review_apipractice.api
+
+class ServerAPI {
+
+//    서버통신을 하게 도와주는 클래스(Retrofit)를 객체로 만들어주는 기능
+
+//    함수 한번만 호출하면, 편하게 가져다 쓸 수 있게 함수로 만들어두자
+
+//    클래스이름.기능() => @classmethod에 대응되는 기능 활용
+    companion object {
+
+
+    }
+}
+
